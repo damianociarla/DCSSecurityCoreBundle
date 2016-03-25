323b93d0d55ed77a1724f5293be60f8c0e755e80
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-03-25 09:21:00 +0100

    Move code in src

--- src/DependencyInjection/DCSSecurityCoreExtension.php
+++ src/DependencyInjection/DCSSecurityCoreExtension.php
@@ -15,7 +15,7 @@ class DCSSecurityCoreExtension extends Extension
 
         $container->setParameter('dcs_security.core.provider_key', $config['provider_key']);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('authentication.xml');
         $loader->load('provider.xml');
     }
